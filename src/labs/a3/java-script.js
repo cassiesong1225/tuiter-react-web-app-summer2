44708ddc9ae8b833b474d011533dbefee74b9e94
Cassie Song <cassiesong@haidegongyuanxiaogezideMacBook-Air.local>
2023-07-22 14:32:55 -0700

    initial3

---
+++ src/labs/a3/java-script.js
@@ -0,0 +1,11 @@
+import VariablesAndConstants from "./variables-and-constants";
+function JavaScript() {
+  console.log('Hello World!');
+  return(
+      <div>
+        <h1>JavaScript</h1>
+        <VariablesAndConstants/>
+      </div>
+  );
+}
+export default JavaScript
