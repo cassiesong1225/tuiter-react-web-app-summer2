d7ac31654e4fdabc24f89dce41ce40353b9aeba2
Cassie Song <cassiesong@haidegongyuanxiaogezideMacBook-Air.local>
2023-07-24 16:30:10 -0700

    JavaScript and React.js Assignment 3 completed

--- src/labs/a3/index.js
+++ src/labs/a3/index.js
@@ -2,6 +2,8 @@ import JavaScript from "./java-script";
 import Classes from "./classes";
 import Styles from "./styles";
 import ConditionalOutput from "./conditional-output";
+import TodoItem from "./todo/todo-item";
+import TodoList from "./todo/todo-list";
 
 function Assignment3() {
   return (
@@ -11,6 +13,8 @@ function Assignment3() {
         <Styles/>
         <Classes/>
         <JavaScript/>
+        <TodoList/>
+        <TodoItem/>
       </div>
   );
 }
